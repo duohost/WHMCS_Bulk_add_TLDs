4cf074ae0699e744a26a35cefac98f15e17db0f0
Sam Craven <sam@seemsgood.com>
2016-01-08 15:30:31 -0600

    v0.1.0 - Basic functionality established
    Details on how this works may be found at
    http://blog.seemsgood.com/bulk-adding-new-tlds-in-whmcs

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# WHMCS-Bulk-add-TLDs
-Adds TLD pricing to WHMCS in pulk
+#WHMCS\_bulk\_add\_TLDs.php v0.1.0
+This script is designed to add pricing for various top level domains (TLDs) in bulk in WHMCS.
+
+WHMCS\_bulk\_add\TLDs.php is refered to as "the script" for the remainder of this README.
+
+Information regarding how the script works may be found [here](http://blog.seemsgood.com/bulk-adding-new-tlds-in-whmcs).
+
+##To use:
+1. Make a backup of your WHMCS database.
+2. Upload WHMCS\_bulk\_add\_TLDs.php to your WHMCS admin directory.
+3. Create/upload a CSV file called tlds.csv in your WHMCS admin directory. (Details below)
+4. Run `php WHMCS_bulk_add_TLDs.php`
+5. In WHMCS, confirm that the pricing has been set as desired.
+
+##tlds.csv
+A sample tlds.csv may be found at `tlds.csv.SAMPLE`. This file is specifically designed such that each TLD features a different configuration that may be used in TLD pricing.
+
+The formatting for tlds.csv is:
+`tld,registration\_price,transfer\_price,renew\_price,minimum\_year,maximum\_year`
+
+All pricing in tlds.csv is on a per-year basis. The price that will be added to WHMCS will be the per-year price multiplied by the number of years being purchased.
+
+To disable domain registrations, set the registration price to `0`.
+
+To disable domain transfers or renewals, set their prices to `-1`.
+
+##Further notes and warnings
+(Most of this is a list of things that the script doesn't do)
+
+**Always back up your database before making changes of any kind.**
+
+The current version of this script has only been tested on WHMCS version 6.2.0, which is the current version as of the time of this file's creation.
+
+The script does not check whether a domain already exists in the database.
+* If you wish to use the script to update a TLD's pricing, you should delete that TLD's pricing from the WHMCS admin interface first.
+
+Domains that are unavailable for one type of purchase but available for other types will show as available in WHMCS. This occurs when the pricing is set via the WHMCS admin area as well and is therefore not an error with the script.
+
+**Always back up your database before making changes of any kind.**
+
+##I want something changed
+* To submit a bug go [here](https://github.com/thecravenone/WHMCS-Bulk-add-TLDs/issues).
+* To submit a complaint, submit a pull request that includes a fix for your complaint.
+
+
+##Credits
+WHMCS\_bulk\_add\_TLDs.php was created by Samuel Craven
+
+Donations may be directed to:
+
+* BTC: 18RfhjtmAiqnuCqhrNPWYtTpyyBCLpHhn
+* PayPal: [Donate](https://blog.seemsgood.com/paypal-donate/)
---
+++ WHMCS_bulk_add_TLDs.php
@@ -0,0 +1,88 @@
+<?php
+/*
+    WHMCS Bulk TLD adder v0.1.0 - adds TLD pricing to WHMCS in bulk
+    Copyright (C) 2016  Samuel Craven
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+require_once('../configuration.php');
+
+$conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$TLD_create=$conn->prepare("INSERT INTO tbldomainpricing (`extension`, `dnsmanagement`, `emailforwarding`, `idprotection`, `eppcode`, `autoreg`, `order`)
+	VALUES (:tld,'','','','','','0')");
+$TLD_get_id=$conn->prepare("SELECT `id` from `tbldomainpricing`
+	WHERE `extension` = :tld");
+$price_insert=$conn->prepare("INSERT INTO tblpricing (`type`, `currency`, `relid`, `msetupfee`, `qsetupfee`, `ssetupfee`, `asetupfee`, `bsetupfee`, `tsetupfee`, `monthly`, `quarterly`, `semiannually`, `annually`, `biennially`, `triennially`)
+	VALUES (:type, '1', :tld_id, :one, :two, :three, :four, :five, '0.00', :six, :seven, :eight, :nine, :ten, '0.00')");
+
+$input = array_map('str_getcsv', file('tlds.csv'));
+foreach ($input as $line) {
+
+	//tld,reg,transfer,renew,minYr,MaxYr
+	$tld 			= $line[0];
+	$reg_price		= $line[1];
+	$transfer_price	= $line[2];
+	$renew_price	= $line[3];
+	$min_year		= $line[4];
+	$max_year		= $line[5];
+
+	//Create the TLD in tbldomainpricing
+	$TLD_create->bindParam(':tld', $tld);
+	$TLD_create->execute();
+
+	//GET TLD'S ID AND ASSIGN IT TO $tld_id
+	$TLD_get_id->bindParam(':tld', $tld);
+	$TLD_get_id->execute();
+	$result = $TLD_get_id->fetch();
+	$tld_id = $result[id];
+
+	//Create an array we'll loop for rather than writing nearly identical code 3 times
+	$type_array = [	["domainregister", $reg_price],
+					["domaintransfer", $transfer_price],
+					["domainrenew", $renew_price] ];
+
+	foreach ($type_array as $entry) {
+		$type = $entry[0];
+		$per_year = $entry[1];
+		//Generate array for price
+		$price_array = [];
+		for($i=1; $i<=10; $i++) {
+			if ($i < $min_year || $i > $max_year) {
+				$price_array[$i] = -1;
+			}
+			else {
+				$price_array[$i] = $per_year * $i;
+			}
+		}
+		$price_insert->bindParam(':type', $type);
+		$price_insert->bindParam(':tld_id', $tld_id);
+		$price_insert->bindParam(':one', $price_array[1]);
+		$price_insert->bindParam(':two', $price_array[2]);
+		$price_insert->bindParam(':three', $price_array[3]);
+		$price_insert->bindParam(':four', $price_array[4]);
+		$price_insert->bindParam(':five', $price_array[5]);
+		$price_insert->bindParam(':six', $price_array[6]);
+		$price_insert->bindParam(':seven', $price_array[7]);
+		$price_insert->bindParam(':eight', $price_array[8]);
+		$price_insert->bindParam(':nine', $price_array[9]);
+		$price_insert->bindParam(':ten', $price_array[10]);
+		$price_insert->execute();
+
+		echo "Added" . $tld . "'s " . $type . "values at " . $per_year . " per year.\n";
+	}
+}
+echo "Completed.\n";
+?>
